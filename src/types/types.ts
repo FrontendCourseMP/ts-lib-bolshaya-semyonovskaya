32a25ea121294219137c053508512d57f0742355
Catzomm <artpogud@gmail.com>
2025-12-01 20:10:37 +0300

    Pogudin

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,49 @@
-export type sum = (a: number, b: number) => number
+export type sum = (a: number, b: number) => number;
+
+export type FormValue = string | number | boolean;
+
+export type FormValues = Record<string, FormValue>;
+
+export type FormField<V = FormValue> = {
+  name: string;
+  value: V;
+  validity: ValidityState;
+};
+
+export type InputMeta =
+  | {
+      type: "text" | "email";
+      required: boolean;
+      minLength?: number;
+      maxLength?: number;
+      pattern?: string;
+    }
+  | {
+      type: "number";
+      required: boolean;
+      min?: number;
+      max?: number;
+      step?: number;
+    }
+  | {
+      type: "checkbox";
+      required: boolean;
+    };
+
+export type FormFieldWithMeta<V = FormValue> = FormField<V> & {
+  meta: InputMeta;
+};
+
+export type FormResult = {
+  values: FormValues;
+  fields: Record<string, FormField>;
+  isValid: boolean;
+};
+
+export type FormResultWithMeta = {
+  values: FormValues;
+  fields: Record<string, FormFieldWithMeta>;
+  isValid: boolean;
+};
+
+export type FormFn = (form: HTMLFormElement) => FormResult | FormResultWithMeta;
